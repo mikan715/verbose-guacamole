f8dc33f16fc5c5a6a645aef34f91281cdb3adeb9
anmink94 <anne.minkenberg_ext@wepa.eu>
2024-09-01 23:43:19 +0200

    adjust backend function to combine fixtures and odds

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ COLLECTION_FIXTURES = 'fixtures'
 COLLECTION_USERS = 'users'
 COLLECTION_BETLOGS = 'betlogs'
 COLLECTION_FIXTUREDFB = 'fixtureDFB'
+COLLECTION_FIXTUREBL = 'fixtureBL'
 
 
 # MongoDB-Client initialisieren
@@ -27,6 +28,7 @@ collection_fixtures = db[COLLECTION_FIXTURES]
 collection_users = db[COLLECTION_USERS]
 collection_betlogs = db[COLLECTION_BETLOGS]
 collection_fixturesDFB = db[COLLECTION_FIXTUREDFB]
+collection_fixturesBL = db[COLLECTION_FIXTUREBL]
 
 def get_mongo_client():
     try:
@@ -63,7 +65,7 @@ def fetch_all_pages(url, headers):
 def get_data_from_db():
     try:
         # Abrufen aller Dokumente aus der Collection
-        data = list(collection_fixturesDFB.find())
+        data = list(collection_fixturesBL.find())
         user = list(collection_users.find())
         print(user)
 
@@ -89,10 +91,10 @@ def get_data_from_db():
 def fetch_combine_store_data():
     try:
         # URLs und Header konfigurieren
-        #fixtures = 'https://v3.football.api-sports.io/fixtures?league=79&season=2024'
-        fixtures = 'https://v3.football.api-sports.io/fixtures?league=81&season=2024'
-        #bets = 'https://v3.football.api-sports.io/odds?league=79&season=2024&bookmaker=27&bet=1'
-        bets = 'https://v3.football.api-sports.io/odds?league=81&season=2024&bookmaker=22&bet=1'
+        fixtures = 'https://v3.football.api-sports.io/fixtures?league=78&season=2024'
+        #fixtures = 'https://v3.football.api-sports.io/fixtures?league=81&season=2024'
+        bets = 'https://v3.football.api-sports.io/odds?league=78&season=2024&bookmaker=27&bet=1'
+        #bets = 'https://v3.football.api-sports.io/odds?league=81&season=2024&bookmaker=22&bet=1'
         headers = {
             'x-rapidapi-host': "v3.football.api-sports.io",
             'x-rapidapi-key': "b65e03f57a3fef50c43dfbff73e002e1"
@@ -101,34 +103,38 @@ def fetch_combine_store_data():
         response1 = requests.get(fixtures, headers=headers)
         response1.raise_for_status()
         all_fixtures = response1.json()['response']
+        #print('all fixtures', all_fixtures)
 
         all_odds = fetch_all_pages(bets, headers)
+        #print('all odds', all_odds)
 
         combined_data = []
         for fixture in all_fixtures:
             fixture_id = fixture['fixture']['id']
+            print('fixture id', fixture_id)
 
             # Suchen der passenden Odds für das Fixture
             matching_odds = next((item for item in all_odds if item['fixture']['id'] == fixture_id), None)
+            print('matiching odds', matching_odds)
 
             cleaned_fixture = {
-                "league": fixture.get("league", {}),  # Leeres Dict als Standardwert
-                "fixture": fixture.get("fixture", {}),  # Leeres Dict als Standardwert
+                "league": fixture.get("league", {}),
+                "fixture": fixture.get("fixture", {}),
                 "teams": fixture.get("teams", {}),
                 "goals": fixture.get("goals", {}),
                 "score": fixture.get("score", {}),
-                "update": fixture.get("update", ""),  # Leerer String als Standardwert
-                "bookmakers": matching_odds.get("bookmakers", []) # Leere Liste, falls keine Odds vorhanden sind
+                "update": fixture.get("update", ""),
+                "bookmakers": matching_odds['bookmakers'] if matching_odds else []
             }
             #print(cleaned_fixture)
             combined_data.append(cleaned_fixture)
 
         # Vor dem Speichern alle Einträge in der Collection löschen
-        #collection_fixturesDFB.delete_many({})
+        collection_fixturesBL.delete_many({})
 
         # Neue Daten in MongoDB speichern
         if combined_data:
-            collection_fixturesDFB.insert_many(combined_data)
+            collection_fixturesBL.insert_many(combined_data)
             return jsonify({"message": "Daten erfolgreich abgerufen, alte Einträge gelöscht und neue gespeichert!"}), 200
         else:
             return jsonify({"message": "Keine Daten zum Speichern."}), 200
