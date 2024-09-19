367c4389defdd6175d5fa657f3efa66ea0051fe7
anmink94 <anne.minkenberg_ext@wepa.eu>
2024-09-19 21:07:09 +0200

    removed frontend

--- path/to/venv/bin/activate.fish
+++ path/to/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mac/Desktop/betgame/path/to/venv"
+set -gx VIRTUAL_ENV "/Users/mac/Desktop/betgame/backend/path/to/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
