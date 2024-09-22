4371eef4c5fbf6b39ecf886abec62079deeb18e2
anmink94 <anne.minkenberg_ext@wepa.eu>
2024-09-23 00:33:35 +0200

    add host

--- main.py
+++ main.py
@@ -283,4 +283,4 @@ def countOdd(wettgeld, oddValue, username, userBalance, bet):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
