0a7b9440ff1d7cd8102d920541e786ef7fa94af5
anmink94 <anne.minkenberg_ext@wepa.eu>
2024-11-02 23:47:08 +0100

    adjust check bet function

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from bson.json_util import dumps
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
 import requests
 import re
 import os
-import schedule
-import time
 from pymongo import MongoClient, errors
 from dotenv import load_dotenv
 
@@ -36,6 +36,10 @@ collection_betlogs = db[COLLECTION_BETLOGS]
 collection_fixturesDFB = db[COLLECTION_FIXTUREDFB]
 collection_fixturesBL = db[COLLECTION_FIXTUREBL]
 
+shared_data_frontend = {
+    "username": "Anne"
+}
+
 def get_mongo_client():
     try:
         client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
@@ -46,26 +50,6 @@ def get_mongo_client():
     except errors.ServerSelectionTimeoutError as err:
         print(f"MongoDB-Verbindung fehlgeschlagen: {err}")
         return None
-    
-
-def fetch_all_pages(url, headers):
-    all_responses = []
-    current_page = 1
-    total_pages = 1
-
-    while current_page <= total_pages:
-        response = requests.get(f"{url}&page={current_page}", headers=headers)
-        response.raise_for_status()
-        data = response.json()
-
-        # Fügen Sie nur die Einträge unter 'response' hinzu
-        all_responses.extend(data.get('response', []))
-
-        # Paging-Informationen aktualisieren
-        current_page = data['paging']['current'] + 1
-        total_pages = data['paging']['total']
-
-    return all_responses
 
 @app.route('/login', methods=['GET'])
 def search():
@@ -79,17 +63,13 @@ def search():
     # Suche nach dem Benutzer in der MongoDB
     user_data = collection_users.find_one({'name': username})
     print(user_data)
-
-    check_bet(username)
     
     if user_data:
-        # MongoDB ObjectId in String umwandeln
+        shared_data_frontend["username"] = username
         user_data['_id'] = str(user_data['_id'])
         return jsonify(user_data), 200
     else:
         return jsonify({'error': 'User not found'}), 404
-    
-    
 
 
 @app.route('/dashboard', methods=['GET'])
@@ -125,8 +105,82 @@ def get_data_from_db():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/addnewuser', methods=['POST'])
+def create_user():
+    data = request.json
+    user = {
+        'name': data.get('name'),
+        'balance': data.get('balance', 0.0),
+        'bets': data.get('bets', []),
+    }
+    user_id = collection_users.insert_one(user).inserted_id
+    return jsonify({'user_id': str(user_id)}), 201
+
+@app.route('/add_bet', methods=['POST'])
+def add_bet():
+    # Daten von der Anfrage abrufen
+    user_id = request.json.get('user_id')
+    fixture = request.json.get('fixture')
+    wettgeld = request.json.get('wettgeld')
+    odd = request.json.get('odd')
+    value = request.json.get('value')
+
+    print(user_id)
+    print(fixture)
+    print(wettgeld)
+    print(odd)
+    print(value)
+
+    # Überprüfen, ob alle Felder vorhanden sind
+    """ if not user_id or not fixture or not wettgeld or not odd or not value:
+        return jsonify({'error': 'All fields (user_id, game, amount, odds) are required'}), 400 """
+
+    # Den Benutzer in der Datenbank finden
+    user = collection_users.find_one({'name': user_id})
+    print(user)
+
+    if not user:
+        return jsonify({'error': 'User not found'}), 404
+
+    # Neue Wette erstellen
+    new_bet = {
+        'fixture': fixture,
+        'wettgeld': wettgeld,
+        'odd': odd,
+        'value': value,
+        'checked_bet': False,
+    }
+    print(new_bet)
+
+    userBalance = user.get('balance')
+    new_balance = float(userBalance) - float(wettgeld)
+
+    # Wetten-Array aktualisieren
+    collection_users.update_one({'name': user_id}, {'$push': {'bets': new_bet}, '$set': {'balance': new_balance}})
+
+    return jsonify({'message': 'Bet added successfully'}), 200
+
+def fetch_all_pages(url, headers):
+    all_responses = []
+    current_page = 1
+    total_pages = 1
+
+    while current_page <= total_pages:
+        response = requests.get(f"{url}&page={current_page}", headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        # Fügen Sie nur die Einträge unter 'response' hinzu
+        all_responses.extend(data.get('response', []))
+
+        # Paging-Informationen aktualisieren
+        current_page = data['paging']['current'] + 1
+        total_pages = data['paging']['total']
+
+    return all_responses
 
 def fetch_combine_store_data():
+    print("fetch new data")
     try:
         # URLs und Header konfigurieren
         fixtures = 'https://v3.football.api-sports.io/fixtures?league=78&season=2024'
@@ -183,64 +237,9 @@ def fetch_combine_store_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-@app.route('/addnewuser', methods=['POST'])
-def create_user():
-    data = request.json
-    user = {
-        'name': data.get('name'),
-        'balance': data.get('balance', 0.0),
-        'bets': data.get('bets', []),
-    }
-    user_id = collection_users.insert_one(user).inserted_id
-    return jsonify({'user_id': str(user_id)}), 201
-
-@app.route('/add_bet', methods=['POST'])
-def add_bet():
-    # Daten von der Anfrage abrufen
-    user_id = request.json.get('user_id')
-    fixture = request.json.get('fixture')
-    wettgeld = request.json.get('wettgeld')
-    odd = request.json.get('odd')
-    value = request.json.get('value')
-
-    print(user_id)
-    print(fixture)
-    print(wettgeld)
-    print(odd)
-    print(value)
-
-    # Überprüfen, ob alle Felder vorhanden sind
-    """ if not user_id or not fixture or not wettgeld or not odd or not value:
-        return jsonify({'error': 'All fields (user_id, game, amount, odds) are required'}), 400 """
-
-    # Den Benutzer in der Datenbank finden
-    user = collection_users.find_one({'name': user_id})
-    print(user)
-
-    if not user:
-        return jsonify({'error': 'User not found'}), 404
-
-    # Neue Wette erstellen
-    new_bet = {
-        'fixture': fixture,
-        'wettgeld': wettgeld,
-        'odd': odd,
-        'value': value,
-    }
-    print(new_bet)
-
-    userBalance = user.get('balance')
-    new_balance = float(userBalance) - float(wettgeld)
-
-    # Wetten-Array aktualisieren
-    collection_users.update_one({'name': user_id}, {'$push': {'bets': new_bet}, '$set': {'balance': new_balance}})
-
-    return jsonify({'message': 'Bet added successfully'}), 200
-
-
-def check_bet(username):
-    user = collection_users.find_one({"name": username})
+def check_bet():
+    print("check bets")
+    user = collection_users.find_one({"name": shared_data_frontend.get("username")})
     userBalance = user.get('balance')
     if not user:
         print("User not found")
@@ -251,6 +250,7 @@ def check_bet(username):
         wettgeld = bet.get('wettgeld')
         oddTeam = bet.get('odd')
         oddValue = bet.get('value')
+        checked_bet = bet.get('checked_bet')
         fixture = collection_fixturesBL.find_one({
             "fixture.id": fixture_id,
             "fixture.status.short": "FT"
@@ -262,14 +262,26 @@ def check_bet(username):
             winner_home = fixture.get('teams', {}).get('home', {}).get('winner')
             winner_away = fixture.get('teams', {}).get('away', {}).get('winner')
 
-            if winner_home == True and oddTeam == "Home":
-                countOdd(wettgeld, oddValue, username, userBalance, bet)
+            if winner_home == True and oddTeam == "Home" and checked_bet == False:
+                countOdd(wettgeld, oddValue, shared_data_frontend.get("username"), userBalance, bet)
+                collection_users.update_one(
+                    {"name": shared_data_frontend.get("username")},
+                    {"$set": {"bets.$[elem].checked_bets": True}}
+                )
                 print('wette gewonnen home')
-            elif winner_away == True and oddTeam == "Away":
-                countOdd(wettgeld, oddValue, username, userBalance, bet)
+            elif winner_away == True and oddTeam == "Away" and checked_bet == False:
+                countOdd(wettgeld, oddValue, shared_data_frontend.get("username"), userBalance, bet)
+                collection_users.update_one(
+                    {"name": shared_data_frontend.get("username")},
+                    {"$set": {"bets.$[elem].checked_bets": True}}
+                )
                 print('wette gewonnen away')
-            elif winner_home != True and winner_away != True and oddTeam == "Draw":
-                countOdd(wettgeld, oddValue, username, userBalance, bet)
+            elif winner_home != True and winner_away != True and oddTeam == "Draw" and checked_bet == False:
+                countOdd(wettgeld, oddValue, shared_data_frontend.get("username"), userBalance, bet)
+                collection_users.update_one(
+                    {"name": shared_data_frontend.get("username")},
+                    {"$set": {"bets.$[elem].checked_bets": True}}
+                )
                 print('wette gewonnen draw')
             else:
                 print('wette nicht gewonnen')
@@ -285,29 +297,22 @@ def countOdd(wettgeld, oddValue, username, userBalance, bet):
         {"name": username},
         {
             "$set": {"balance": new_balance},
-            "$pull": {"bets": bet}
         }
     )
     print(f"User won the bet. New balance: {new_balance}")
 
-schedule.every().hour.do(fetch_combine_store_data)
-schedule.every().hour.do(check_bet)
 
+def meine_funktion():
+    print("Funktion wurde ausgeführt.")
 
-def activate_scheduler():
-    """Flask Hook, um den Scheduler beim ersten Request zu starten."""
-    print("Scheduler gestartet...")
-    
-    def run_scheduler():
-        """Scheduler-Schleife innerhalb der Flask-Anwendung."""
-        while True:
-            schedule.run_pending()
-            time.sleep(1)  # Zeitintervall zwischen Task-Prüfungen
-    
-    # In die Schleife gehen und den Scheduler ausführen
-    run_scheduler()
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(fetch_combine_store_data, 'interval', hours=1)
+    scheduler.add_job(check_bet, 'interval', minutes=1)
+    scheduler.start()
+    print("Scheduler gestartet.")
 
 if __name__ == '__main__':
-    activate_scheduler()
+    start_scheduler()
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 8000)))
 
